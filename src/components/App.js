df7bfb2131e6b6774cf50cd41f9d63f27b06e4e7
pratik <pratikgaikwad551@gmail.com>
2021-05-30 18:22:20 +0530

    added time interval

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,28 @@ import React, { Component, useState, useEffect } from "react";
 import '../styles/App.css';
 
 const App = () => {
-  // write your code here 
+  // write your code here
+  
+  const [currentTime,setcurrentTime]=useState(0);
+  
+  
+  function updateTime(){
+    var input=document.getElementById("timeCount").value;
+    console.log(input);
+    setInterval(()=>{
+        setcurrentTime(input--);
+    },1000);
+    
+  }
 
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={updateTime} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{currentTime}</div>
     </div>
   )
 }
